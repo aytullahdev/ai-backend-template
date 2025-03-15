fbc31a1b10098adf8cc1569f301a3f6ad76bbb32
ayat-delineate <ayat@delineate.pro>
2025-03-16 01:00:12 +0600

    bun runtime added

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ app.use("*", supabaseMiddleware());
 
 app.get("/", async (c) => {
   const supabse = getSupabase(c);
-
   return c.text("Hello Hono!");
 });
 
 // Mount the auth router
 app.route("/auth", authRouter);
 
-export default app;
+export default {
+  fetch: app.fetch,
+  port: 3000,
+};
